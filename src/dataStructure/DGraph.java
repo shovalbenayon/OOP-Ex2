84b6f554b2cb940ad2e103127df8d8ceff5282b9
shoval benayon <shovalbb14@gmail.com>
2019-12-23 15:15:42 +0200

    Add classes EdgeNode and DataNode, starting DGraph

--- src/dataStructure/DGraph.java
+++ src/dataStructure/DGraph.java
@@ -1,48 +1,85 @@
 package dataStructure;
 
 import java.util.Collection;
+import java.util.HashMap;
 
 public class DGraph implements graph{
+	private HashMap<Integer , node_data> Nodes_Map = new HashMap<>();
+	private HashMap<Integer , HashMap<Integer , edge_data>> Edges_Map = new HashMap<>();
+	private int NM_size;
+	private int EM_size;
+	private int MC;
 
-	@Override
+	/**
+	 * this method returns the node data by the key
+	 * @param key - the node_id
+	 * @return node data
+	 */
 	public node_data getNode(int key) {
-		// TODO Auto-generated method stub
+		if (Nodes_Map.containsKey(key))
+			return this.Nodes_Map.get(key);
 		return null;
 	}
 
-	@Override
+	/**
+	 * this method returns the edge of 2 nodes data only if the source node exists
+	 * @param src the source node
+	 * @param dest the destination node
+	 * @return
+	 */
 	public edge_data getEdge(int src, int dest) {
-		// TODO Auto-generated method stub
+		if (Nodes_Map.containsKey(src))
+			return this.Edges_Map.get(src).get(dest);
 		return null;
 	}
 
-	@Override
+	/**
+	 * this method add a node to the nodes map if the node isn't existing yet
+	 * @param n the node to add
+	 */
 	public void addNode(node_data n) {
-		// TODO Auto-generated method stub
-		
+		if (this.Nodes_Map.get(n) ==  null) {
+			this.Nodes_Map.put(n.getKey(), n);
+			NM_size++;
+			MC++;
+		}
+		else {
+			System.out.println("Node already exist");
+		}
 	}
 
 	@Override
 	public void connect(int src, int dest, double w) {
-		// TODO Auto-generated method stub
-		
+		if (this.getNode(src) != null && this.getNode(dest)!= null ){
+			edge_data t = new EdgeNode(src , dest , w);
+			if (this.getEdge(src , dest) == null) {
+				if (this.Edges_Map.get(src) == null) {
+					this.Edges_Map.put(src, new HashMap<Integer, edge_data>());
+					this.Edges_Map.get(src).put(dest, t);
+				}
+				else
+					this.Edges_Map.get(src).put(dest, t);
+			}
+		}
+		else {
+			throw new NullPointerException("ERR:Not Existing Nodes");
+		}
+		EM_size++;
+		MC++;
 	}
 
 	@Override
 	public Collection<node_data> getV() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.Nodes_Map.values();
 	}
 
 	@Override
 	public Collection<edge_data> getE(int node_id) {
-		// TODO Auto-generated method stub
-		return null;
+		return this.Edges_Map.get(node_id).values();
 	}
 
 	@Override
 	public node_data removeNode(int key) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
@@ -54,20 +91,17 @@ public class DGraph implements graph{
 
 	@Override
 	public int nodeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return NM_size;
 	}
 
 	@Override
 	public int edgeSize() {
-		// TODO Auto-generated method stub
-		return 0;
+		return EM_size;
 	}
 
 	@Override
 	public int getMC() {
-		// TODO Auto-generated method stub
-		return 0;
+		return MC;
 	}
 
 }
---
+++ src/dataStructure/DataNode.java
@@ -0,0 +1,80 @@
+package dataStructure;
+
+import utils.Point3D;
+
+public class DataNode implements node_data {
+    private int Key;
+    private double Weight;
+    private Point3D Location;
+    private String Info;
+    private int tag;
+
+    public DataNode(){
+        this.Key = 0;
+        this.Weight = 0;
+        this.Location = null;
+        this.Info = null;
+        this.tag = 0;
+    }
+
+    public DataNode(int key , double weight , Point3D l , String info , int tag){
+        this.Key = key;
+        this.Weight = weight;
+        this.Location = new Point3D(l.x() , l.y() , l.z());
+        this.Info = info;
+        this.tag = tag;
+    }
+
+    public DataNode(DataNode oth){
+        this.Key = oth.Key;
+        this.tag = oth.tag;
+        this.Weight = oth.Weight;
+        this.Info = oth.Info;
+        this.Location= new Point3D(oth.Location);
+    }
+
+    @Override
+    public int getKey() {
+        return this.Key;
+    }
+
+    @Override
+    public Point3D getLocation() {
+        return this.Location;
+    }
+
+    @Override
+    public void setLocation(Point3D p) {
+        this.Location = new Point3D(p.x() , p.y() , p.z());
+    }
+
+    @Override
+    public double getWeight() {
+        return this.Weight;
+    }
+
+    @Override
+    public void setWeight(double w) {
+        this.Weight = w;
+    }
+
+    @Override
+    public String getInfo() {
+        return this.Info;
+    }
+
+    @Override
+    public void setInfo(String s) {
+        this.Info = s;
+    }
+
+    @Override
+    public int getTag() {
+        return this.tag;
+    }
+
+    @Override
+    public void setTag(int t) {
+        this.tag = t;
+    }
+}
